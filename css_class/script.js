177af5f07db1199183c15ba01fc71a2728c878f6
PaoloBSK <0219468@up.edu.mx>
2022-08-19 20:00:28 -0500

    Update

---
+++ css_class/script.js
@@ -0,0 +1,20 @@
+//legado
+var var1 = "var1"
+//no s epuede cambiar el valor
+const const1 = "const1"
+let let1 = "let1" //se usa si va a mutar
+let = 14
+
+console.log("let1", let1)
+console.log(let1)
+console.warn("const1, let1",const1, let1)
+
+//legado
+function name(params)
+{
+
+}
+const myFunction = (txt) => {
+    console.log(txt, ("from func"))
+}
+myFunction("hola")
